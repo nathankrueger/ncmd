8a18fccc7988bab35ec7b9bb0278ee3bd4fd2e73
Nathan Krueger <natekrueger805@gmail.com>
2016-01-04 21:58:26 -0800

    Updating fileops to return status

--- ncmd_fileops.py
+++ ncmd_fileops.py
@@ -16,6 +16,7 @@ def isDirectory(path):
 	return (not os.path.isfile(path) and os.path.exists(path))
 
 def copy(src, dest):
+	result = True
 	try:
 		if isFile(src):
 			shutil.copy(src, dest)
@@ -24,15 +25,23 @@ def copy(src, dest):
 		np.print_msg("Copied {0} to {1}...".format(src, dest), ErrorLevel.INFO)
 	except Exception as err:
 		np.print_msg("Failed to copy {0} to {1}...\n\t{2}".format(src, dest, err), ErrorLevel.ERROR)
+		result = False
+	
+	return result
 
 def move(src, dest):
+	result = True
 	try:
 		shutil.move(src, dest)
 		np.print_msg("Moved {0} to {1}...".format(src, dest), ErrorLevel.INFO)
 	except Exception as err:
 		np.print_msg("Failed to move {0} to {1}...\n\t{2}".format(src, dest, err), ErrorLevel.ERROR)
+		result = False
+
+	return result
 
 def remove(target):
+	result = True
 	try:
 		if isFile(target):
 			os.remove(target)
@@ -41,3 +50,7 @@ def remove(target):
 		np.print_msg("Moved {0} to {1}...".format(src, dest), ErrorLevel.INFO)
 	except Exception as err:
 		np.print_msg("Failed to move {0} to {1}...\n\t{2}".format(src, dest, err), ErrorLevel.ERROR)
+		result = False
+
+	return result
+
